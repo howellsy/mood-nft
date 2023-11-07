9ec8298ddc60e1567112fefe33acdb4f311b7e4b
howellsy <jamie.howells@gmail.com>
2023-11-07 16:59:14 +0000

    Add flipMood() function with owner check

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -5,6 +5,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    error MoodNft__OnlyOwnerCanFlipMood();
+
     uint256 private s_tokenCounter;
     string private s_happySvgImageUri;
     string private s_sadSvgImageUri;
@@ -26,6 +28,23 @@ contract MoodNft is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter += 1;
+    }
+
+    function isApprovedOrOwner(address spender, uint256 tokenId) public view returns (bool) {
+        address owner = ownerOf(tokenId);
+        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+    }
+
+    function flipMood(uint256 tokenId) public {
+        // only the NFT owner should be able to flip the mood
+        if (!isApprovedOrOwner(msg.sender, tokenId)) {
+            revert MoodNft__OnlyOwnerCanFlipMood();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
