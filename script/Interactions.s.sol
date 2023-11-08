3445a69d7e3c95cadd452d2a7a63afbf9d21be86
howellsy <jamie.howells@gmail.com>
2023-11-08 14:42:06 +0000

    Script for minting and flipping a Mood NFT

---
+++ script/Interactions.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployedBasicNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
+    }
+
+    function mintNftOnContract(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    uint256 public constant TOKEN_ID_TO_FLIP = 0;
+
+    function run() external {
+        address mostRecentlyDeployedBasicNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        flipMoodNft(mostRecentlyDeployedBasicNft);
+    }
+
+    function flipMoodNft(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).flipMood(TOKEN_ID_TO_FLIP);
+        vm.stopBroadcast();
+    }
+}
