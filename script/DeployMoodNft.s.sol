a3dcabcc73821a0b6aceea54f8fda69183aece4e
howellsy <jamie.howells@gmail.com>
2023-11-08 10:03:45 +0000

    Deploy script for MoodNft

---
+++ script/DeployMoodNft.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNft is Script {
+    function run() external returns (MoodNft) {
+        string memory happySvg = vm.readFile("./img/happy.svg");
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(svgToImageURI(happySvg), svgToImageURI(sadSvg));
+        vm.stopBroadcast();
+
+        return moodNft;
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(svg));
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
